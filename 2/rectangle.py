e0a26001fbf84352849ae261dc78749ab465726e
ktnlvr <artur.roos.artur@gmail.com>
2024-08-22 09:55:44 +0300

    Expect correct input in exercise 2/3

--- 2/rectangle.py
+++ 2/rectangle.py
@@ -1,4 +1,6 @@
 if __name__ == '__main__':
     width = float(input("width = "))
-    length = float(input("height = "))
+    length = float(input("length = "))
+    assert width >= 0
+    assert length >= 0
     print(f"P = {2*width + 2*length}\tS = {width*length}")
